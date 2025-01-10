73bced926a62c1dddea2bb6e57fea6e55c7398b6
Konstantin Burkalev <KSDaemon@gmail.com>
2025-01-10 19:36:38 +0200

    pass SchemaRef to load methods.

--- rust/cubesql/cubesql/src/compile/engine/df/scan.rs
+++ rust/cubesql/cubesql/src/compile/engine/df/scan.rs
@@ -665,6 +665,7 @@ impl ExecutionPlan for CubeScanExecutionPlan {
                 self.auth_context.clone(),
                 self.transport.clone(),
                 meta.clone(),
+                self.schema.clone(),
                 self.options.clone(),
                 self.wrapped_sql.clone(),
             )
@@ -847,6 +848,7 @@ async fn load_data(
     auth_context: AuthContextRef,
     transport: Arc<dyn TransportService>,
     meta: LoadRequestMeta,
+    schema: SchemaRef,
     options: CubeScanOptions,
     sql_query: Option<SqlQuery>,
 ) -> ArrowResult<V1LoadResult> {
@@ -878,7 +880,7 @@ async fn load_data(
         )
     } else {
         let result = transport
-            .load(span_id, request, sql_query, auth_context, meta)
+            .load(span_id, request, sql_query, auth_context, meta, schema)
             .await;
         let mut response = result.map_err(|err| ArrowError::ComputeError(err.to_string()))?;
         if let Some(data) = response.results.pop() {
@@ -906,6 +908,7 @@ fn load_to_stream_sync(one_shot_stream: &mut CubeScanOneShotStream) -> Result<()
     let auth = one_shot_stream.auth_context.clone();
     let transport = one_shot_stream.transport.clone();
     let meta = one_shot_stream.meta.clone();
+    let schema = one_shot_stream.schema.clone();
     let options = one_shot_stream.options.clone();
     let wrapped_sql = one_shot_stream.wrapped_sql.clone();
 
@@ -917,6 +920,7 @@ fn load_to_stream_sync(one_shot_stream: &mut CubeScanOneShotStream) -> Result<()
             auth,
             transport,
             meta,
+            schema,
             options,
             wrapped_sql,
         ))
@@ -1401,6 +1405,7 @@ mod tests {
                 _sql_query: Option<SqlQuery>,
                 _ctx: AuthContextRef,
                 _meta_fields: LoadRequestMeta,
+                _schema: SchemaRef,
             ) -> Result<V1LoadResponse, CubeError> {
                 let response = r#"
                     {
